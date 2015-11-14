0402c51ff9ca9bb0c570a20770881e50f9734d2f
Franz Beltran <franzbeltran79@gmail.com>
2015-11-14 16:45:59 -0400

    Terminar registro y sesion

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def home
+    @user = User.new
+  end
+
+  def sign_up
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to profile_path(@user.name)
+    else
+      render :home
+    end
+
+  end
+
+  def sign_in
+    user = User.find_by(:email =>params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to profile_path(user.name)
+    end
+    # render(:text => "Falta un template :)")
+  end
+  def profile
+    # render(:text => params[:name])
+    @user = User.find_by_name(params[:name])
+  end
+
+  def sign_out
+    log_out
+    redirect_to home_url
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
